458fa91b5669d8f0ef7a4e6401dae47df7c4d272
Bruno Guimarães <brunogdepaula@hotmail.com>
2023-12-14 09:35:13 -0300

    Projeto completo

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import './global.css';
+
+// import { PI as numeroPi, GTM_BRASIL } from './teste'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
